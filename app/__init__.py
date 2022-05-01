7562937ab7df399363be95e954d3c5a002a9907d
yoshi <yn55@njit.edu>
2022-05-01 12:57:43 -0400

    Test #1

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,46 @@
 """A simple flask web app"""
-import os
+import flask_login
 from flask import Flask, render_template
-from app.cli import create_database
-from app.db import db
+from flask_bootstrap import Bootstrap5
+from flask_wtf.csrf import CSRFProtect
+
+import os
+from flask import Flask
+from app.context_processors import utility_text_processors
+from app.simple_pages import simple_pages
+from app.auth import auth
+from app.exceptions import http_exceptions
 from app.db.models import User
+from app.db import db
+from app.auth import auth
+from app.cli import create_database
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
 
-
-def page_not_found():
+login_manager = LoginManager()
+def page_not_found(e):
     return render_template("404.html"), 404
-
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
 
-    app.register_error_handler(404, page_not_found)
+    bootstrap = Bootstrap5(app)
+    app.register_blueprint(simple_pages)
+    app.register_blueprint(auth)
+    app.context_processor(utility_text_processors)
+    app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'flatly'
+    app.register_error_handler(404,page_not_found)
+    #app.add_url_rule("/", endpoint="index")
     db_dir = "database/db.sqlite"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -23,8 +48,12 @@ def create_app():
     # add command function to cli commands
     app.cli.add_command(create_database)
 
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
 
     return app
+
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,24 +1,19 @@
 from datetime import datetime
 
+from flask_login._compat import unicode
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import db
 from flask_login import UserMixin
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(300), nullable=False, unique=True)
-    about = db.Column(db.String(300), nullable=True, unique=False)
     authenticated = db.Column(db.Boolean, default=False)
     registered_on = db.Column('registered_on', db.DateTime)
-    active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
-    is_admin = db.Column('is_admin', db.Boolean(), nullable=False, server_default='0')
-
-    # `roles` and `groups` are reserved words that *must* be defined
-    # on the `User` model to use group- or role-based authorization.
 
     def __init__(self, email, password):
         self.email = email
@@ -35,7 +30,7 @@ class User(UserMixin, db.Model):
         return False
 
     def get_id(self):
-        return self.id
+        return unicode(self.id)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -44,4 +39,4 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password, password)
 
     def __repr__(self):
-        return '<User %r>' % self.email
+        return '<User %r>' % self.username
